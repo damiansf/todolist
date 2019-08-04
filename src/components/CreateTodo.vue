a5428a288b4b7f2d73fb855cba79680e8ba996d0
Damian Sandhu-Franceschi <damiansandhu-franceschi@Damians-MacBook-Pro.local>
2019-08-03 23:21:01 -0400

    adding logic

---
+++ src/components/CreateTodo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <input v-model="title" placeholder="Add Todo" v-on:keyup.enter="create">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CreateTodo',
+  data() {
+    return {
+      title: '',
+    };
+  },
+  methods: {
+    create() {
+      let title = this.title;
+      let id = this.$uuid.v1();
+      if(this.title.length > 0) {
+        this.$emit('create-todo', {
+          title: title,
+          done: false,
+          id: id,
+        })
+        this.title = ''
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
